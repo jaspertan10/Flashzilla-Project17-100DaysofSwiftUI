b53d243b76176534f5a822d4014f85661510f0fe
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2025-03-18 22:08:10 -0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Jasper Tan on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
