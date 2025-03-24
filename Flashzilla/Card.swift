e1a351d07221933ab2c1d86f5088f3f0e7eeee0c
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2025-03-23 23:46:47 -0700

    Add CardView, Implemented cardview in ContentView with 10 example cards, enabled swipe to remove flashcard

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Jasper Tan on 3/23/25.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor who?", answer: "Jodie Whittaker")
+}
